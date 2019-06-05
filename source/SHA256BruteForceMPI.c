aea7feb79205e453de2a1c7cc1797be576934a30
Philipp Ensinger <philippensi2@gmail.com>
2019-06-05 16:07:19 +0200

    Changed Order of Functions

--- source/SHA256BruteForceMPI.c
+++ source/SHA256BruteForceMPI.c
@@ -10,6 +10,28 @@ int main(int argc, char** argv) {
 	int length;
 	char *charset;
 	char *hash;
+
+	// Initialize the MPI environment
+	MPI_Init(NULL, NULL);
+
+	// Get the number of processes
+	int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+	// Get the rank of the process
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	// Get the name of the processor
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
+	int name_len;
+	MPI_Get_processor_name(processor_name, &name_len);
+
+	// Print off a hello world message
+	printf("SHA256BruteForce HELLO from processor %s, rank %d"
+		" out of %d processors\n", processor_name, world_rank, world_size);
+
+
   
 	while ((opt = getopt(argc, argv, "l:c:")) != -1){
 		switch (opt){
@@ -34,26 +56,9 @@ int main(int argc, char** argv) {
 	strcpy(hash, argv[optind]);
 	printf("Submitted Hash: %s\n", hash);
 
-	// Initialize the MPI environment
-	MPI_Init(NULL, NULL);
-
-	// Get the number of processes
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-	// Get the rank of the process
-	int world_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-	// Get the name of the processor
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
-
-	// Print off a hello world message
-	printf("SHA256BruteForce HELLO from processor %s, rank %d"
-		" out of %d processors\n", processor_name, world_rank, world_size);
-
 	// Finalize the MPI environment.
 	MPI_Finalize();
+
+	free(charset);
+	free(hash);
 }
