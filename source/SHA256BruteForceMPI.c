e867ee925665fe33e78097e94163030685f1eaf4
Philipp Ensinger <philippensi2@gmail.com>
2019-06-06 22:23:14 +0200

    test

--- source/SHA256BruteForceMPI.c
+++ source/SHA256BruteForceMPI.c
@@ -26,7 +26,6 @@ int main(int argc, char** argv) {
 	// Get the rank of the process
 	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
 	// Get the name of the processor
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
 	int name_len;
