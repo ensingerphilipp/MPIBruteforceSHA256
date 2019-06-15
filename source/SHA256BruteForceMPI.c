a839758ddf2b952fd407af4a5ce571953057a4a6
Philipp Ensinger <philippensi2@gmail.com>
2019-06-16 00:30:41 +0200

    test

--- source/SHA256BruteForceMPI.c
+++ source/SHA256BruteForceMPI.c
@@ -176,6 +176,7 @@ void bruteForceSha256(char* charset, char* splitCharset, unsigned char* hashHex,
 		if (!recvComplete){
 			MPI_Test(&recvRequest, &recvComplete, &recvStatus);
 		} else {
+			printf("Node %d break", world_rank);
 			break;
 		}
 
